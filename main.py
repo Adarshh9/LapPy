b372722bb545fe1e3627fb33d716b6a3275768bf
Adarsh <akesherwani900@gmail.com>
2024-04-26 20:18:51 +0530

    updated main.py

--- main.py
+++ main.py
@@ -1,11 +1,7 @@
-import numpy as np
-from flask import Flask, request, jsonify, render_template, url_for
-import pickle
+from flask import Flask, request, jsonify, render_template
 from utils import get_utils,predict_price
 
 app = Flask(__name__)
-# pipe = pickle.load(open('pipe.pkl','rb'))
-# df = pickle.load(open('df.pkl','rb'))
 
 @app.route('/')
 def home():
@@ -39,19 +35,7 @@ def predict():
     })
     response.headers.add('Access-Control-Allow-Origin', '*')
 
-    # return render_template('home.html', prediction_text="AQI for Jaipur {}".format(prediction[0]))
     return response
-
-@app.route('/predict_api',methods=['POST'])
-def predict_api():
-    '''
-    For direct API calls trought request
-    '''
-    data = request.get_json(force=True)
-    prediction = model.predict([np.array(list(data.values()))])
-
-    output = prediction[0]
-    return jsonify(output)
 
 if __name__ == '__main__':
     app.run(debug=True ,port=5000)
